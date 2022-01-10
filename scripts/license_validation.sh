413a69ca6c1f6e401e2417568f17450690ee8e63
Ganesh-Yeole <92855225+Ganesh-Yeole@users.noreply.github.com>
2022-01-10 14:11:51 +0530

    Update license_validation.sh

--- scripts/license_validation.sh
+++ scripts/license_validation.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/Ganesh-Yeole/quickstart-boomi-gke-molecule/main/scripts/requirements.txt
-wget https://raw.githubusercontent.com/Ganesh-Yeole/quickstart-boomi-gke-molecule/main/scripts/license_validation.py
+#wget https://raw.githubusercontent.com/Ganesh-Yeole/quickstart-boomi-gke-molecule/main/scripts/license_validation.py
 
 pip install -t . -r ./requirements.txt
 
-if [ $BOOMIAUTHENTICATIONTYPE == "token" ]
-then
-    result=`python license_validation.py "$MOLECULEACCOUNTID" "BOOMI_TOKEN.$MOLECULEUSERNAME" "$BOOMIMFAAPITOKEN" MOLECULE 60`
-else
-    result=`python license_validation.py "$MOLECULEACCOUNTID" "$MOLECULEUSERNAME" "$MOLECULEPASSWORD" MOLECULE 60`
-fi
+/usr/bin/python3 license_validation.py "accentureengineeringmaste-25T6QY" "g.madhukar.yeole@accenture.com" "NamuCheeky@305" MOLECULE 60
 
 status=`echo $result|cut -d, -f1|awk -F':' '{ print $2 }'`
 token=`echo $result|cut -d, -f2|awk -F':' '{ print $2 }'`
 
-echo \{\"license_validation\":\"$status\"\, \"installation_token\":\"$token\"\} > $AZ_SCRIPTS_OUTPUT_PATH
+echo \{\"license_validation\":\"$status\"\, \"installation_token\":\"$token\"\} 
