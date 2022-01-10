9308e0922598eb21ef28017156641439609ddf7c
Ganesh-Yeole <92855225+Ganesh-Yeole@users.noreply.github.com>
2022-01-10 14:49:52 +0530

    Update license_validation.sh

--- scripts/license_validation.sh
+++ scripts/license_validation.sh
@@ -5,7 +5,7 @@ wget https://raw.githubusercontent.com/Ganesh-Yeole/quickstart-boomi-gke-molecul
 
 pip install -t . -r ./requirements.txt
 
-/usr/bin/python3 license_validation.py "accentureengineeringmaste-25T6QY" "g.madhukar.yeole@accenture.com" "NamuCheeky@305" MOLECULE 60
+result=`/usr/bin/python3 license_validation.py "accentureengineeringmaste-25T6QY" "g.madhukar.yeole@accenture.com" "NamuCheeky@305" MOLECULE 60`
 
 status=`echo $result|cut -d, -f1|awk -F':' '{ print $2 }'`
 token=`echo $result|cut -d, -f2|awk -F':' '{ print $2 }'`
